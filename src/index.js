a072200b837a4dce26f4830efcba2160af2272fc
hryder <r37he@edu.uwaterloo.ca>
2021-03-04 19:39:40 -0500

    basic layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js'
 
 ReactDOM.render(
   <React.StrictMode>
